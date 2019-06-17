1763be48b28b47c792bfdfc6e6fc4260a9da8140
singhpawan501 <51296968+singhpawan501@users.noreply.github.com>
2019-06-17 05:47:23 +0530

    Initial commit

---
+++ dice rolling simulator.py
@@ -0,0 +1,11 @@
+import random as r
+key='yes'
+min=1
+max=6
+while key=='yes' or key=='y' :
+    rand=r.randint(min,max)
+    print("dice has been rolling .....")
+    print("your value is : ",rand)
+    key=input("DO YOU WANT TO ROLL AGAIN...")
+print("THANKS FOR ROLLING THE DICE")    
+    
